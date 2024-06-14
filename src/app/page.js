e1239cb74f71b1ff38e14a98036ad30bb268b3f7
abdurrashidkhan <abdurrashidkhan.personal@gmail.com>
2024-06-14 16:15:02 +0600

    checking error

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import OurClient from "@/Components/OurClient/OurClient";
 import OurTeam from "@/Components/OurTeam/OurTeam";
 import Pricing from "@/Components/Pricing/Pricing";
 import Subscribe from "@/Components/Subscribe/Subscribe";
-import Team from "@/Components/Team/team";
+import Team from "@/Components/Team/Team";
 import WorkingProcess from "../Components/workingProcess/workingProcess";
 import About from "./about/page";
 import Projects from "./projects/page";
