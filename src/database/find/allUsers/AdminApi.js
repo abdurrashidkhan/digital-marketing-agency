ebabcc0992849f5331bd430e6bb55c8ffb3357ff
abdurrashidkhan <abdurrashidkhan.personal@gmail.com>
2024-06-14 09:51:40 +0600

    n/c

--- src/database/find/allUsers/AdminApi.js
+++ src/database/find/allUsers/AdminApi.js
@@ -1,7 +1,7 @@
 
 export default async function AdminApi(email) {
   const res = await fetch(`http://localhost:3000/api/users/${email}/`, {
-    cache: "no-store",
+    cache: 'no-store'
   });
   return res.json();
 }
