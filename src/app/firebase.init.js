ff52f7dcc5bdd35d7865e15eabb621e6d37ced08
abdurrashidkhan <abdurrashidkhan.destop@gmail.com>
2024-07-02 17:34:42 +0600

    firease account change

--- src/app/firebase.init.js
+++ src/app/firebase.init.js
@@ -1,17 +1,17 @@
+import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTi-pzoucAq0FMhcaQvOrJwmZjGfHssxE",
-  authDomain: "e-merge-marketing.firebaseapp.com",
-  projectId: "e-merge-marketing",
-  storageBucket: "e-merge-marketing.appspot.com",
-  messagingSenderId: "247855149176",
-  appId: "1:247855149176:web:91c6d043b24012c7b87d8d"
+  apiKey: "AIzaSyBLNPRzI8taZZVljNKM6qYoLfbRHTMWzrI",
+  authDomain: "digital-marketing-agency-30fa0.firebaseapp.com",
+  projectId: "digital-marketing-agency-30fa0",
+  storageBucket: "digital-marketing-agency-30fa0.appspot.com",
+  messagingSenderId: "151543460035",
+  appId: "1:151543460035:web:8d165e4c3dc630d9443276"
 };
 
 // Initialize Firebase
